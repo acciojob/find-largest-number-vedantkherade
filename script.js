238d224bc6e5194a6f296e785ca046674182fce9
Vedant Kherade <152008775+vedantkherade@users.noreply.github.com>
2025-12-12 16:54:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function findLargest(a, b, c) {
     return c;
   }
 }
-
+  
 const num1 = parseInt(prompt("Enter First Number."));
 const num2 = parseInt(prompt("Enter Second Number."));
 const num3 = parseInt(prompt("Enter Third Number."));
